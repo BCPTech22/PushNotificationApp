7fcd11bcd94167f0cc296bd53549d04019d3bdc8
Othmane Habib Allah <habiballahothmane@gmail.com>
2022-03-28 09:42:42 +0000

    First Commit

---
+++ src/screens/SignInScreen/SignInScreen.js
@@ -0,0 +1,106 @@
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  useWindowDimensions,
+  ScrollView,
+  TextInput,
+} from 'react-native';
+import Logo from '../../../assets/images/Logo_BCP.png';
+import CustomInput from '../../components/CustomInput';
+import CustomButton from '../../components/CustomButton';
+import SocialSignInButtons from '../../components/SocialSignInButtons';
+import {useNavigation} from '@react-navigation/native';
+import {useForm, Controller} from 'react-hook-form';
+
+const SignInScreen = () => {
+  /*const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');*/
+
+  const {height} = useWindowDimensions();
+  const navigation = useNavigation();
+  const {
+    control,
+    handleSubmit,
+    formState: {errors},
+  } = useForm();
+
+  const onSignInPressed = data => {
+    console.log(data);
+    // validate user
+    navigation.navigate('Home');
+  };
+
+  const onForgotPasswordPressed = () => {
+    navigation.navigate('ForgotPassword');
+  };
+
+  const onSignUpPress = () => {
+    navigation.navigate('SignUp');
+  };
+
+  return (
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <View style={styles.root}>
+        <Image
+          source={Logo}
+          style={[styles.logo, {height: height * 0.3}]}
+          resizeMode="contain"
+        />
+
+        <CustomInput
+          name="username"
+          placeholder="Username"
+          control={control}
+          rules={{required: 'Username is required'}}
+        />
+        <CustomInput
+          name="password"
+          placeholder="Password"
+          secureTextEntry
+          control={control}
+          rules={{
+            required: 'Password is required',
+            minLength: {
+              value: 12,
+              message: 'Password should be minimum 12 characters long',
+            },
+          }}
+        />
+
+        <CustomButton text="Sign In" onPress={handleSubmit(onSignInPressed)} />
+
+        <CustomButton
+          text="Forgot password?"
+          onPress={onForgotPasswordPressed}
+          type="TERTIARY"
+        />
+
+        <SocialSignInButtons />
+
+        <CustomButton
+          text="Not yet have an account? Create one"
+          onPress={onSignUpPress}
+          type="TERTIARY"
+        />
+      </View>
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  root: {
+    alignItems: 'center',
+    padding: 20,
+    backgroundColor: '#dce0de',
+  },
+  logo: {
+    width: '70%',
+    maxWidth: 300,
+    maxHeight: 200,
+  },
+});
+
+export default SignInScreen;
